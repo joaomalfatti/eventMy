3e4a100bb3748f749cdf5fae02a24d2c5305d0c2
joaomalfatti <joao.cursos1210@gmail.com>
2024-08-11 20:46:55 -0300

    [New] - Styles were created first, to then see the logical part of the Home Index. Now let's configure Home.

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,56 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #131016;
+  padding: 24px;
+`;
+
+export const EventName = styled.View`
+  color: #FFFFFF;
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 46%;
+`;
+
+export const EventDate = styled.View`
+  color: #6b6b6b;
+  font-size: 16px;
+`;
+
+export const EventInput = styled.View`
+  height: 56px;
+  background-color: #1F1E25;
+  border-radius: 5;
+  color: "#FFFFFF";
+  padding: 16px;
+  flex: 1;
+  margin-right: 12;
+`;
+
+export const ButtonText = styled.View`
+  color: #FFF;
+  font-size: 18px;
+`;
+
+export const ButtonTouch = styled.View`
+  width: 56px;
+  height: 56px;
+  border-radius: 5;
+  background-color: #31CF67;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ViewForm = styled.View`
+  width: 100%;
+  flex-direction: row;
+  margin-top: 36px;
+  margin-bottom: 42px;
+`;
+
+export const ListEmptyText = styled.View`
+  color: #FFFFFF;
+  font-size: 16px;
+  text-align: center;
+`;
